7d8a1e06c277bea99b915a77c158e06aa29cb052
Lukas Romsicki <lukas@romsicki.com>
2020-04-21 20:46:31 -0400

    Initial commit

---
+++ app/graphql/forecast_server_schema.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+class ForecastServerSchema < GraphQL::Schema
+  query(Types::QueryType)
+
+  # Opt in to the new runtime (default in future graphql-ruby versions)
+  use GraphQL::Execution::Interpreter
+  use GraphQL::Analysis::AST
+
+  # Add built-in connections for pagination
+  use GraphQL::Pagination::Connections
+end
---
+++ app/graphql/types/base_interface.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+module Types
+  module BaseInterface
+    include GraphQL::Schema::Interface
+
+    field_class Types::BaseField
+  end
+end
