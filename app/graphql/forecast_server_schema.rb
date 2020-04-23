be35c3ab9bc10109080b6a7341307e985ba3673b
Lukas Romsicki <lukas@romsicki.com>
2020-04-23 14:49:00 -0400

    Disable introspection in prod

--- app/graphql/forecast_server_schema.rb
+++ app/graphql/forecast_server_schema.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 class ForecastServerSchema < GraphQL::Schema
+  disable_introspection_entry_points if Rails.env.production?
+
   query(Types::QueryType)
 
   # Opt in to the new runtime (default in future graphql-ruby versions)
