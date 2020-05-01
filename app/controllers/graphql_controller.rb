9d13698b20003d8bd304c7f9eca40614dd1ec7d4
Lukas Romsicki <lukas@romsicki.com>
2020-05-01 18:47:01 -0400

    Rename Clouds to Nimbus

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -13,7 +13,7 @@ class GraphqlController < ApplicationController
       # Query context goes here, for example:
       # current_user: current_user,
     }
-    result = CloudsServerSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
+    result = NimbusServerSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render(json: result)
   rescue => e
     raise e unless Rails.env.development?
--- app/graphql/nimbus_server_schema.rb
+++ app/graphql/nimbus_server_schema.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-class CloudsServerSchema < GraphQL::Schema
+class NimbusServerSchema < GraphQL::Schema
   disable_introspection_entry_points if Rails.env.production?
 
   query(Types::QueryType)
