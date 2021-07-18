29f29662143e89614a3e5c6733244d2343e81115
Lukas Romsicki <3951690+lfroms@users.noreply.github.com>
2021-07-17 22:09:34 -0400

    Convert database to PostGIS (#16)

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2020_04_23_174152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "postgis"
 
   create_table "canada_sites", id: false, force: :cascade do |t|
     t.string "name"
